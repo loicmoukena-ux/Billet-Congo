5cd86583987a6d6039755a84db0297d6705f9537
Loïc MOUKENA <loicmoukena@gmail.com>
2026-04-28 03:38:52 +0100

    Debut Design

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@prisma/client', '.prisma/client'],
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
